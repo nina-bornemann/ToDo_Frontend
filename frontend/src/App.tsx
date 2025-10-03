6d7e3e3474e1db717ff1759bc2d7d9e9a2a56219
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-03 02:59:19 -0700

    simple initial HTML setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className={"flex flex-col gap-16"}>
+            <h1>Super Kanban</h1>
+            <div className={"flex flex-row justify-between"}>
+                <button><h2>Home</h2></button>
+                <button><h2>ToDo</h2></button>
+                <button><h2>Doing</h2></button>
+                <button><h2>Done</h2></button>
+            </div>
+
+            <div className={"flex flex-row m-16 gap-16"}>
+                <div>
+                    <h3>ToDo</h3>
+                    <div>
+                        <div className={"flex flex-col gap-4 rounded-2xl bg-zinc-700 w-72"}>
+                            <p>Description</p>
+                            <p>Status</p>
+                            <button className={"mx-16"}>Details</button>
+                            <button className={"mx-16"}>Advance</button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h3>Doing</h3>
+                    <div>
+                        <div className={"flex flex-col gap-4 rounded-2xl bg-zinc-700 w-72"}>
+                            <p>Description</p>
+                            <p>Status</p>
+                            <button className={"mx-16"}>Details</button>
+                            <button className={"mx-16"}>Advance</button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h3>Done</h3>
+                    <div>
+                        <div className={"flex flex-col gap-4 rounded-2xl bg-zinc-700 w-72"}>
+                            <p>Description</p>
+                            <p>Status</p>
+                            <button className={"mx-16"}>Details</button>
+                            <button className={"mx-16"}>Advance</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <input type={"text"} placeholder={"Enter your ToDo"}/>
+                <button>Add</button>
+            </div>
+        </div>
     </>
   )
 }
