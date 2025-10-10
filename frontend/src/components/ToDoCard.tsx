3cb2e529a716b00ce8f5571e15c75dfbc158f671
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-10 14:31:05 -0700

    add ToDo model and ToDoCard

---
+++ frontend/src/components/ToDoCard.tsx
@@ -0,0 +1,13 @@
+import type { ToDo } from "../model/ToDo";
+
+type ToDoCardProps = {
+    todo: ToDo;
+};
+
+export default function ToDoCard({ todo }: Readonly<ToDoCardProps>) {
+    return (
+        <div className="border rounded p-2 m-2 bg-gray-100 shadow-sm">
+            <p>{todo.description}</p>
+        </div>
+    );
+}
