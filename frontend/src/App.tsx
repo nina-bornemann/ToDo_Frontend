800b23587b01c68e937fa6266dfd11750a860681
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-10 14:18:38 -0700

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,10 @@
-import { Route, Routes } from "react-router-dom";
 import './App.css'
-import Card from "./Card.tsx";
-import NavBar from "./NavBar.tsx";
-import {allTodos} from "./ToDoModel.ts";
 
 function App() {
 
-    let todoCards = allTodos
-        .map(todo => <Card description={todo.description} status={todo.status}/>)
-
-
-    return (
+  return (
     <>
-        <div className={"flex flex-col gap-16"}>
 
-            <NavBar/>
-
-            <Routes>
-            <Route path={"/home"} element={
-                <div className={"flex flex-row m-16 gap-16"}>
-                    <div>
-                        <h3>ToDo</h3>
-                        <div>
-                            {todoCards}
-                        </div>
-                    </div>
-                    <div>
-                        <h3>Doing</h3>
-                        <div>
-                            <Card description={"test-3"} status={"doing"}/>
-
-                        </div>
-                    </div>
-                    <div>
-                        <h3>Done</h3>
-                        <div>
-                            <Card description={"test-3"} status={"done"}/>
-                        </div>
-                    </div>
-                </div>
-            } />
-
-                <Route path={"/todo"} />
-                <Route path={"/doing"}/>
-                <Route path={"/done"}/>
-
-            </Routes>
-
-            <div>
-                <input type={"text"} placeholder={"Enter your ToDo"}/>
-                <button>Add</button>
-            </div>
-        </div>
     </>
   )
 }
