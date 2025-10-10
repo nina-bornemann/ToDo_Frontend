07dc76bb92a237c0f15c45ecb19e08d9c866f51e
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-10 14:41:24 -0700

    filter todos in ToDoCard and add Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import ToDoBoard from "./components/ToDoBoard";
 import './App.css'
 
 function App() {
 
   return (
     <>
+        <nav className="flex gap-4 p-4 bg-gray-100 shadow">
+            <Link to="/">Home</Link>
+            <Link to="/board">ToDo Board</Link>
+        </nav>
 
+        <Routes>
+            <Route path="/" element={<h1>Welcome to your Kanban Board</h1>} />
+            <Route path="/board" element={<ToDoBoard />} />
+        </Routes>
     </>
   )
 }
