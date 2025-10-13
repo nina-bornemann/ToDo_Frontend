808b7a763db591b2b2f0406d23a4b5ca7acaa317
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-12 23:51:29 -0700

    initial commit with function header and useStates

--- frontend/src/components/ToDoCard.tsx
+++ frontend/src/components/ToDoCard.tsx
@@ -1,4 +1,6 @@
 import type { ToDo } from "../model/ToDo";
+import axios from "axios";
+import {useState} from "react";
 
 type ToDoCardProps = {
     todo: ToDo;
@@ -6,12 +8,18 @@ type ToDoCardProps = {
 
 export default function ToDoCard({ todo }: Readonly<ToDoCardProps>) {
 
+    const [updatedDescription, setUpdatedDescription] = useState<string>("")
+    const [updatedStatus, setUpdatedStatus] = useState<string>("")
+
+    function handleChange() {
+        axios.put("api/todo/:id")
+    }
 
     return (
         <>
             <div className={"card"}>
                 <p>{todo.description}</p>
-                <button>Edit</button>
+                <button onClick={handleChange}>Edit</button>
                 <button>Advance</button>
             </div>
         </>
