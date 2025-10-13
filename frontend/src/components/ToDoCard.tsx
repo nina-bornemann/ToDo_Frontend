9f2e9a251c2bebe98d149ccc8356712798dbf260
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-13 11:18:45 -0700

    add initial PUT request for status editing

--- frontend/src/components/ToDoCard.tsx
+++ frontend/src/components/ToDoCard.tsx
@@ -11,13 +11,25 @@ export default function ToDoCard(props: Readonly<ToDoCardProps>) {
     const [updatedDescription, setUpdatedDescription] = useState<string>(props.todo.description)
     const [updatedStatus, setUpdatedStatus] = useState<ToDo["status"]>(props.todo.status)
     const [updatedToDo, setUpdatedToDo] = useState(props.todo)
-    const [isEditing, setIsEditing] = useState<boolean>(false)
+    const [isDescriptionEditing, setIsDescriptionEditing] = useState<boolean>(false)
+    const [isStatusEditing, setIsStatusEditing] = useState<boolean>(false)
 
     function handleDescriptionChange() {
         axios
             .put(`/api/todo/${props.todo.id}`, {id:props.todo.id, description: updatedDescription, status: updatedStatus})
             .then((r) => {
                 setUpdatedDescription(r.data.description)
+                setUpdatedToDo({id: props.todo.id, description: updatedDescription, status:updatedStatus})
+                console.log(updatedToDo)
+            })
+            .catch((e) => console.log(e))
+    }
+
+    function handleStatusChange() {
+        axios
+            .put(`/api/todo/${props.todo.id}`, {id:props.todo.id, description: updatedDescription, status: updatedStatus})
+            .then((r) => {
+                setUpdatedStatus(r.data.status)
                 setUpdatedToDo({id: props.todo.id, description: updatedDescription, status:props.todo.status})
                 console.log(updatedToDo)
             })
@@ -27,18 +39,21 @@ export default function ToDoCard(props: Readonly<ToDoCardProps>) {
     return (
         <>
             <div className={"card"}>
-                {!isEditing && <p>{updatedDescription}</p>}
+                {!isDescriptionEditing && <p>{updatedDescription}</p>}
 
-                {isEditing && <input className={"cardInputField"}
+                {isDescriptionEditing && <input className={"cardInputField"}
                                      type={"text"}
                                      name={"input"}
                                      value={updatedDescription}
                                      placeholder={"Enter adjusted description"}
                                      onChange={(e) => setUpdatedDescription(e.target.value)}/>}
+
                 <div className={"cardButtons"}>
-                {!isEditing && <button onClick={() => setIsEditing(!isEditing)}>Edit ✏️</button>}
-                {isEditing && <button onClick={() => {handleDescriptionChange(); setIsEditing(!isEditing)}}>Save</button>}
-                <button>Advance</button>
+                    {!isDescriptionEditing && <button onClick={() => setIsDescriptionEditing(!isDescriptionEditing)}>Edit ✏️</button>}
+                    {isDescriptionEditing && <button onClick={() => {handleDescriptionChange(); setIsDescriptionEditing(!isDescriptionEditing)}}>Save</button>}
+
+                    {!isStatusEditing && <button onClick={() => setIsStatusEditing(!isStatusEditing)}>Advance ⏭️</button>}
+                    {isStatusEditing && <button onClick={() => {handleStatusChange(); setIsStatusEditing(!isStatusEditing)}}>Save</button>}
                 </div>
             </div>
         </>
