6b9f97a64b959dfa627008a41a9ffbf5579469c0
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-13 10:41:46 -0700

    add PUT functionality for description editing

--- frontend/src/components/ToDoCard.tsx
+++ frontend/src/components/ToDoCard.tsx
@@ -6,20 +6,37 @@ type ToDoCardProps = {
     todo: ToDo;
 };
 
-export default function ToDoCard({ todo }: Readonly<ToDoCardProps>) {
+export default function ToDoCard(props: Readonly<ToDoCardProps>) {
 
-    const [updatedDescription, setUpdatedDescription] = useState<string>("")
-    const [updatedStatus, setUpdatedStatus] = useState<string>("")
+    const [updatedDescription, setUpdatedDescription] = useState<string>(props.todo.description)
+    const [updatedStatus, setUpdatedStatus] = useState<ToDo["status"]>(props.todo.status)
+    const [updatedToDo, setUpdatedToDo] = useState(props.todo)
+    const [isEditing, setIsEditing] = useState<boolean>(false)
 
-    function handleChange() {
-        axios.put("api/todo/:id")
+    function handleDescriptionChange() {
+        axios
+            .put(`/api/todo/${props.todo.id}`, {id:props.todo.id, description: updatedDescription, status: updatedStatus})
+            .then((r) => {
+                setUpdatedDescription(r.data.description)
+                setUpdatedToDo({id: props.todo.id, description: updatedDescription, status:props.todo.status})
+                console.log(updatedToDo)
+            })
+            .catch((e) => console.log(e))
     }
 
     return (
         <>
             <div className={"card"}>
-                <p>{todo.description}</p>
-                <button onClick={handleChange}>Edit</button>
+                {!isEditing && <p>{updatedDescription}</p>}
+
+                {isEditing && <input type={"text"}
+                                     name={"input"}
+                                     value={updatedDescription}
+                                     placeholder={"Enter adjusted description"}
+                                     onChange={(e) => setUpdatedDescription(e.target.value)}/>}
+
+                {!isEditing && <button onClick={() => setIsEditing(!isEditing)}>Edit ✏️</button>}
+                {isEditing && <button onClick={() => {handleDescriptionChange(); setIsEditing(!isEditing)}}>Save</button>}
                 <button>Advance</button>
             </div>
         </>
